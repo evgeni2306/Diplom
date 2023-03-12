cb55a31c47b50a775e3b9a867a405fc05a8f003b
Evgeni2306 <Evgeni2306@outlook.com>
2023-03-12 12:22:46 +0500

    Перенос функционала  юзер меню в хедер

--- resources/views/Components/Header/Header.blade.php
+++ resources/views/Components/Header/Header.blade.php
@@ -13,21 +13,21 @@
 
                 <li class="menu__item">
                     <a href={{route('interviewDirection')}}>
-                        <div class="linkItem" >
+                        <div class="linkItem">
                             Тренировочное собеседование
                         </div>
                     </a>
                 </li>
                 <li class="menu__item">
                     <a href={{route('getProfessionsForKnowledgeBase')}}>
-                        <div class="linkItem" >
+                        <div class="linkItem">
                             База знаний
                         </div>
                     </a>
                 </li>
                 <li class="menu__item">
                     <a href={{route('expansionContent')}}>
-                        <div class="linkItem" >
+                        <div class="linkItem">
                             СРК
                         </div>
                     </a>
@@ -35,13 +35,33 @@
 
             </ul>
         </nav>
-        <nav class="header__user">
-{{--            {/*<img*/}--}}
-{{--            {/*    className="header__user-img"*/}--}}
-{{--            {/*    src="/img/user.png"*/}--}}
-{{--            {/*    alt="user"*/}--}}
-{{--            {/*/>*/}--}}
-{{--            {/*<span className="header__user-arrow"></span>*/}--}}
+        <nav class="user-menu">
+            <ul class="user-menu__list">
+                <li class="user-name">
+                    <img class="avatar" src="{{"/Pages/Interview/InterviewPreview/svg/favourites.svg"}}" alt="user">
+                    <a href="#" class="user-menu__link" tabindex="1"></a><span class="menu__arrow"></span>
+                    <ul class="sub-menu__list">
+
+                        <li><a href="" class="sub-menu__link">Мой профиль</a>
+                        </li>
+                        <li><a href="" class="sub-menu__link">Поиск</a>
+                        </li>
+                        <li><a href="" class="sub-menu__link">Настройки</a>
+                        </li>
+                        <li><a href="" class="sub-menu__link">Выйти</a></li>
+
+
+                        {{--                        <li><a href="" class="sub-menu__link"><img src="/PageMap/img/user/01.svg" alt="">Мой профиль</a></li>--}}
+                        {{--                        <li><a href="" class="sub-menu__link"><img src="/PageMap/img/icons/search-icon.svg" alt="">Поиск</a></li>--}}
+                        {{--                        <li><a href="" class="sub-menu__link"><img src="/PageMap/img/user/02.svg" alt="">Настройки</a></li>--}}
+                        {{--                        <li><a href="" class="sub-menu__link"><img src="/PageMap/img/user/03.svg" alt="">Выйти</a></li>--}}
+
+                    </ul>
+                </li>
+            </ul>
         </nav>
     </div>
 </div>
+<!--------------MENU-------------------->
+<script src="{{"/Pages/Components/Header/header.js"}}"></script>
+<!--------------MENU-------------------->
