af3ad5d417521f34b34a1f184b688712bad08218
Evgeni2306 <Evgeni2306@outlook.com>
2023-04-14 21:38:42 +0500

    Исправление базы знаний

--- resources/views/KnowledgeBase/KnowledgeBase.blade.php
+++ resources/views/KnowledgeBase/KnowledgeBase.blade.php
@@ -2,35 +2,35 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8"/>
-    <title>Knowledge Base</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{"/common/css/base.css"}}">
     <link rel="stylesheet" href="{{"/Pages/Components/Header/styles.css"}}"/>
     <link rel="stylesheet" href="{{"/Pages/KnowledgeBase/styles.css"}}"/>
+    <title>Knowledge Base</title>
 </head>
 <body>
 <!--------------HEADER-------------------->
 @include('Components.Header.Header')
 <!--------------/HEADER-------------------->
-<div class="knowledge-base">
+<div class="container">
 
-    <h1 class="knowledge-base__title">База знаний</h1>
-    <p class="knowledge-base__subtitle">
+    <h1 class="container-title">База знаний</h1>
+    <p class="container-subtitle">
         Знаете вопрос, который могут задать на собеседовании, но его нет в базе?
-        <span class="knowledge-base__link">Пополнить базу знаний</span>
+        <span class="container-link">Пополнить базу знаний</span>
     </p>
-    <div class="knowledge-base__container">
-        <div class="knowledge-base__professions">
-            <div class="knowledge-base__professions-found">
-                <input class="" required list="brow" value="" id="profession"
+    <div class="knowledge-base">
+        <div class="choose-profession-block">
+                <input class="choose-profession" required list="brow" value="" id="profession"
                        placeholder="Выберите профессию" onchange="changeProf()">
                 <datalist class="search-box" id="brow">
                     @foreach($professions as $item)
                         <option value="{{$item->name}}">
                     @endforeach
                 </datalist>
-            </div>
         </div>
-        <div class="knowledge-base__questions" id="questionBox">
+        <div class="knowledge-base-questions" id="questionBox">
         </div>
     </div>
 </div>
@@ -69,33 +69,33 @@
     }
 
     function divBuilder(question, i) {
-        const element = '<div class="knowledge-base__question" name="number">' +
-            '<div class="knowledge-base__question-top">' +
-            '<div class="knowledge-base__question-top-tag">' +
+        const element = '<div class="knowledge-base-question" name="number">' +
+            '<div class="knowledge-base-question-top">' +
+            '<div class="knowledge-base-question-top-tag">' +
             question.category +
             '</div>' +
             '<div id="isFavorite" class="hidden">' + question.isFavorite + '</div>' +
             '<div id="favoriteId" class="hidden">' + question.favoriteId + '</div>' +
             '<div id="questionId" class="hidden">' + question.questionId + '</div>' +
 
-            '<div id="nonFavorite" class="knowledge-base__question-top-favourites hidden">' +
-            '<button class="knowledge-base__question-top-favourites__btn" onclick="addFavorite(' + i + ')">' +
-            '<div class="knowledge-base__question-top-favourites__icon">' +
+            '<div id="nonFavorite" class="knowledge-base-question-top-favourites hidden">' +
+            '<button class="knowledge-base-question-top-favourites-btn" onclick="addFavorite(' + i + ')">' +
+            '<div class="knowledge-base-question-top-favourites-icon">' +
             '<img src="{{"/common/svg/emptyFavourites.svg"}}" alt="favourites" width="16px" height="15px"/>' +
             '</div>' +
             '</button>' +
             '</div>' +
 
-            '<div id="Favorite" class="knowledge-base__question-top-favourites hidden">' +
-            '<button class="knowledge-base__question-top-favourites__btn" onclick="deleteFavorite(' + i + ')">' +
-            '<div class="knowledge-base__question-top-favourites__icon">' +
+            '<div id="Favorite" class="knowledge-base-question-top-favourites hidden">' +
+            '<button class="knowledge-base-question-top-favourites-btn" onclick="deleteFavorite(' + i + ')">' +
+            '<div class="knowledge-base-question-top-favourites-icon">' +
             '<img src="{{"/common/svg/fillFavourites.svg"}}" alt="favourites" width="16px" height="15px"/>' +
             '</div>' +
             '</button>' +
             '</div>' +
 
             '</div>' +
-            '<div class="knowledge-base__question__body">' +
+            '<div class="knowledge-base-question-body">' +
             question.question;
         '</div>' +
         '</div>'
