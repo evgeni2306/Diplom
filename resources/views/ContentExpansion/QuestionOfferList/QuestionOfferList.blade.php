9ca91cec7f6a78f6c1cce873378adf32580c4887
Evgeni2306 <Evgeni2306@outlook.com>
2023-04-25 21:11:36 +0500

    Готовая страница списка с предложенными вопросами

--- resources/views/ContentExpansion/QuestionOfferList/QuestionOfferList.blade.php
+++ resources/views/ContentExpansion/QuestionOfferList/QuestionOfferList.blade.php
@@ -2,39 +2,60 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8"/>
-    <title></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{"/common/css/base.css"}}">
     <link rel="stylesheet" href="{{"/Pages/Components/Header/styles.css"}}"/>
     <link rel="stylesheet" href="{{"/Pages/ContentExpansion/QuestionOfferList/styles.css"}}"/>
-    <link rel="stylesheet" href="{{"/Pages/ContentExpansion/common/styles.css"}}"/>
+    <title>Система расширения контента</title>
 </head>
 <body>
 <!--------------HEADER-------------------->
 @include('Components.Header.Header')
 <!--------------/HEADER-------------------->
 <div class="container">
-    <div class="form__container">
-        Система расширения контента
-        <div class="grid">
-            <div class="gridItem">Категория</div>
-            <div class="gridItem">Вопрос</div>
-            <div class="gridItem">Статус</div>
-            <div class="gridItem">Действия</div>
-        </div>
+    <h1 class="container__title">Система расширения контента</h1>
+    <p class="container-subtitle">
+        <a href="{{route('questionOfferForm')}}" class="container-link">Предложить вопрос для добавления</a>
+    </p>
+    @if(count($offers)>0)
         @foreach($offers as $item)
-            <div class="grid">
-                <div class="gridItem">{{$item->category->name}}</div>
-                <div class="gridItem">{{$item->question}}</div>
-                <div class="gridItem {{$item->status}}">{{$item->statusName}}</div>
-                <div class="gridItem">
-                    <a class="viewButton" href="{{route('questionOfferView',$item->id)}}">См</a>
-                    @if($item->status == "Green")
-                        <a class="hideButton" href="{{route('questionOfferVisible',$item->id)}}">Скр</a></div>
-                @endif
+            <div class="questionBlock">
+                <div class="questionBlockTop">
+                    <div class="categoryBlock">
+                        <div class="category">{{$item->category->name}}</div>
+                    </div>
+                    <div class="questionStatusBlock">
+                        <div class="status {{$item->status}} ">{{$item->statusName}}</div>
+                    </div>
+                    <div class="questionActionsBlock">
+                        <div class="actions">
+                            <a class="actionButton" href="{{route('questionOfferView',$item->id)}}"><img
+                                    class="actionButton"
+                                    src="{{"/Pages/ContentExpansion/QuestionOfferList/svg/openEye.svg"}}"
+                                    alt=""></a>
+                            @if($item->status == "Green")
+                                <a class="actionButton" href="{{route('questionOfferVisible',$item->id)}}"><img
+                                        src="{{"/Pages/ContentExpansion/QuestionOfferList/svg/closeEye.svg"}}"
+                                        alt=""></a>
+                            @endif
+                        </div>
+                    </div>
+
+                </div>
+                <div class="questionBlockBody">
+                    <div class="question flex-elem">{{$item->question}}</div>
+                </div>
             </div>
         @endforeach
-        <a href="{{route('questionOfferForm')}}">Добавить вопрос</a>
-    </div>
+    @endif
+    @if(count($offers)===0)
+
+        <div class="voidMenu">
+            <div class="voidTextMain">Тут будут отображаться предложенные вами вопросы</div>
+            <div class="voidTextSecond">Но пока что тут пусто</div>
+        </div>
+    @endif
 </div>
 </body>
 </html>
