be2b4077704acb7fff10414a4bc7678ae2a65d80
Evgeni2306 <Evgeni2306@outlook.com>
2023-04-28 18:16:32 +0500

    Адаптив страницы итогов симуляции

--- resources/views/Interview/InterviewResults/InterviewResults.blade.php
+++ resources/views/Interview/InterviewResults/InterviewResults.blade.php
@@ -2,99 +2,107 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8"/>
-    <title>Interview Results</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{"/common/css/base.css"}}">
     <link rel="stylesheet" href="{{"/Pages/Components/Header/styles.css"}}"/>
     <link rel="stylesheet" href="{{"/Pages/Interview/InterviewResults/styles.css"}}"/>
+    <title>Результаты симуляции</title>
 </head>
 <body onLoad="load()">
 <!--------------HEADER-------------------->
 @include('Components.Header.Header')
 <!--------------/HEADER-------------------->
 
-<div className="results-page">
-    <h1 class="results-page__title">Результат собеседования</h1>
-    <div class="results-page__container">
-        <h1 class="results-page__title">Результат собеседования</h1>
-        <div class="results-page__progress">
-            <div class="results-page__progress-top">
-                <div class="results-page__progress-top-name">
-                    Правильные ответы
-                </div>
-                <div class="results-page__progress-top-count">
-                    {{$results->countRight}}/{{$results->countRight + $results->countWrong}}
-                </div>
+<div class="container">
+    <h1 class="containerTitle">Результат собеседования</h1>
+    <div class="progressSector">
+        <div class="progressSectorTop">
+            <div class="progressSectorTopTitle">
+                Правильные ответы
             </div>
-            <div class="results-page__progress-bar">
-                <div class="results-page__progress-bar-done">
-                    <div class="percent-count">
-                    </div>
-                </div>
+            <div class="progressSectorTopCount">
+                {{$results->countRight}}/{{$results->countRight + $results->countWrong}}
             </div>
         </div>
-        <h2 class="results-page__title-sec">
-            Вам следует обратить внимание на эти вопросы
-        </h2>
+        <div class="progress">
+            <div class="progressBar">
+                {{$results->countRight / ($results->countRight + $results->countWrong) * 100}}%
+            </div>
+        </div>
+    </div>
+    <h2 class="containerTitle">
+        Вам следует обратить внимание на эти вопросы
+    </h2>
 
-        <div class="results-page__questions">
-
-            @for($i=0; $i<count($results->wrongQuestions); $i++)
-                <?$item = $results->wrongQuestions[$i]?>
-                <div id="{{$i}}" name="number" class="results-page__question">
-                    <div id="isFavorite" class="hidden">{{$item->isFavorite}}</div>
-                    <div id="favoriteId" class="hidden">{{$item->favoriteId}}</div>
-                    <div id="questionId" class="hidden">{{$item->questionId}}</div>
-                    <div class="results-page__question-top">
-                        <div class="results-page__question-top-tag">
-                            {{$item->category}}
-                        </div>
-                        <div id="nonFavorite" class="hidden">
-                            <div class="results-page__question-top-favourites">
-                                <div class="results-page__question-top-favourites__icon">
-                                    <img src="{{"/common/svg/emptyFavourites.svg"}}"
-                                         alt="favourites" width="16px" height="15px"/>
-                                </div>
-                                <button class="results-page__question-top-favourites__btn"
-                                        onclick="addFavorite('{{$i}}')">
-                                    Добавить в избранное
-                                </button>
+    <div class="questionSector">
+        @for($i=0; $i<count($results->wrongQuestions); $i++)
+                <? $item = $results->wrongQuestions[$i] ?>
+            <div id="{{$i}}" name="number" class="questionBlock">
+                <div id="isFavorite" class="hidden">{{$item->isFavorite}}</div>
+                <div id="favoriteId" class="hidden">{{$item->favoriteId}}</div>
+                <div id="questionId" class="hidden">{{$item->questionId}}</div>
+                <div class="questionTop">
+                    <div class="questionTag">
+                        {{$item->category}}
+                    </div>
+                    <div id="nonFavorite" class="hidden">
+                        <div class="questionFavorite">
+                            <div class="questionFavoriteIcon">
+                                <img src="{{"/common/svg/emptyFavourites.svg"}}"
+                                     alt="favourites" width="16px" height="15px"/>
                             </div>
-                        </div>
-                        <div id="Favorite" class="hidden">
-                            <div class="results-page__question-top-favourites">
-                                <div class="results-page__question-top-favourites__icon">
-                                    <img src="{{"/common/svg/fillFavourites.svg"}}"
-                                         alt="favourites" width="16px" height="15px"/>
-                                </div>
-                                <button class="results-page__question-top-favourites__btn"
-                                        onclick="deleteFavorite('{{$i}}')">
-                                    Добавлено в избранное
-                                </button>
-                            </div>
+                            <button class="questionFavoriteButton"
+                                    onclick="addFavorite('{{$i}}')">
+                                Добавить в избранное
+                            </button>
                         </div>
                     </div>
-                    <div class="results-page__question__body">
-                        {{$item->question}}
+                    <div id="Favorite" class="hidden">
+                        <div class="questionFavorite">
+                            <div class="questionFavoriteIcon">
+                                <img src="{{"/common/svg/fillFavourites.svg"}}"
+                                     alt="favourites" width="16px" height="15px"/>
+                            </div>
+                            <button class="questionFavoriteButton"
+                                    onclick="deleteFavorite('{{$i}}')">
+                                Добавлено в избранное
+                            </button>
+                        </div>
                     </div>
-
+                </div>
+                <div class="questionBody">
+                    {{$item->question}}
                 </div>
 
-            @endfor
+            </div>
 
-        </div>
-        {{--                <div id ="wrongQuestions" hidden>{{$results->wrongQuestions}}</div>--}}
-        <a href={{route("interviewPreview", $results->professionId)}}>
-            <button class="primary-button">
-                Попробовать еще раз
-            </button>
-        </a>
+        @endfor
 
     </div>
+    {{--                <div id ="wrongQuestions" hidden>{{$results->wrongQuestions}}</div>--}}
+    <a href={{route("interviewPreview", $results->professionId)}}>
+        <button class="primary-button">
+            Попробовать еще раз
+        </button>
+    </a>
+
 </div>
+
 </body>
 <script>
 
+    function progressDiagram() {
+        let points = {{$results->countRight / ($results->countRight + $results->countWrong) * 100}};
+        const progress = document.querySelector('.progressBar');
+        setTimeout(() => {
+            progress.style.opacity = 1;
+            progress.style.width = points + '%';
+        })
+    }
+
     function load() {
+        progressDiagram()
         questions = <? echo $results->wrongQuestions ?>;
         const x = document.getElementsByName("number")
         for (i = 0; i < questions.length; i++) {
